9d06cabaac3d6bba72735a22e43c44e449564901
Matheus Gabriel <math.gsilva07@gmail.com>
2023-10-25 09:44:44 -0300

    Create Veiculo.java

---
+++ Veiculos/Veiculo.java
@@ -0,0 +1,54 @@
+public abstract class Veiculo {
+    private String marca;
+    private String modelo;
+    private int ano;
+    double combustivel;
+
+    public Veiculo(String marca, String modelo, int ano, double combustivel) {
+        this.marca = marca;
+        this.modelo = modelo;
+        this.ano = ano;
+        this.combustivel = combustivel;
+    }
+
+    /* Getters & Setters */
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public int getAno() {
+        return ano;
+    }
+
+    public void setAno(int ano) {
+        this.ano = ano;
+    }
+
+    public double getCombustivel() {
+        return combustivel;
+    }
+
+    public void setCombustivel(double combustivel) {
+        this.combustivel = combustivel;
+    }
+
+    /* Métodos gerais */
+
+    // Default method
+    public double calcularConsumo() {
+        return getCombustivel() * 5;
+    }
+}
